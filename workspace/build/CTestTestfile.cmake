263038826d18802e24b29e8cd8ab5ca06e1ece97
lleejk96 <lleejk96@koreatech.ac.kr>
2021-11-21 14:56:43 +0900

    workspace

---
+++ workspace/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/lee/workspace/src
+# Build directory: /home/lee/workspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtlebot3")
---
+++ workspace/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/lee/workspace/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/lee/lee_ws/devel;/opt/ros/noetic'.split(';') if '/home/lee/lee_ws/devel;/opt/ros/noetic' != '' else []
